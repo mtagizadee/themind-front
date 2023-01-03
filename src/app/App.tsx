6be55340aea1542dbca64a4880148b15ccab5e89
mtaghizada <magatagi0@gmail.com>
2023-01-03 16:15:01 +0400

    eslint was added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,11 @@
+import React from "react";
+
 const App = () => {
-  return <div className="flex items-center justify-center h-screen w-screen"> TheMind </div>;
+  return (
+    <div className="flex items-center justify-center h-screen w-screen font-bold text-xl">
+      <div>Init</div>
+    </div>
+  );
 };
 
 export default App;
