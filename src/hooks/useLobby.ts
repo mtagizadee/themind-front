a3b476b0ec3b3359d7763bad2fc976ebfdfde301
mtaghizada <magatagi0@gmail.com>
2023-01-08 23:31:27 +0400

    useLobby hook was added

--- src/hooks/useLobby.ts
+++ src/hooks/useLobby.ts
@@ -1,17 +1,20 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
+import { LobbiesController } from "../api";
+import { TLobby } from "../common/types";
 import useLoading from "./useLoading";
 
 const useLobby = (id: string, onError: (error: any) => void) => {
-  const [lobby, setLobby] = useState();
+  const [lobby, setLobby] = useState<TLobby>({} as any);
   const { execute, isLoading } = useLoading(async () => {
-
+    const lobby = await LobbiesController.getOne(id);
+    setLobby(lobby);
   }, onError);
-  
+
   useEffect(() => {
     execute();
   }, [id]);
 
-  return (  );
-}
- 
+  return { lobby, isLoading };
+};
+
 export default useLobby;
