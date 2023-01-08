0e6d2885ab0ba764da261da2dd37876614ad8d7e
mtaghizada <magatagi0@gmail.com>
2023-01-08 23:44:15 +0400

    useFetch hook was added to the useLobby hook

--- src/hooks/useLobby.ts
+++ src/hooks/useLobby.ts
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 import { LobbiesController } from "../api";
 import { lobbyCleaner, TLobby } from "../common/types";
-import useLoading from "./useLoading";
+import useFetch from "./useFetch";
 
 type TUseLobbyResponse = {
   lobby: TLobby;
-  isLoading: boolean;
+  lobbyLoading: boolean;
+  lobbyError: any;
 };
 
 /**
@@ -14,22 +15,22 @@ type TUseLobbyResponse = {
  * @param onError - error handler function that will be called if something goes wrong
  * @returns TUseLobbyResponse - lobby object and isLoading flag
  */
-const useLobby = (id: string, onError: (error: any) => void): TUseLobbyResponse => {
+const useLobby = (id: string): TUseLobbyResponse => {
   const [lobby, setLobby] = useState<TLobby>(lobbyCleaner());
-  const { execute, isLoading } = useLoading(async () => {
+  const [fetchLobby, lobbyLoading, lobbyError] = useFetch(async () => {
     const lobby = await LobbiesController.getOne(id);
     setLobby(lobby);
-  }, onError);
+  });
 
   useEffect(() => {
-    execute();
+    fetchLobby();
 
     return () => {
       setLobby(lobbyCleaner());
     };
   }, [id]);
 
-  return { lobby, isLoading };
+  return { lobby, lobbyLoading, lobbyError };
 };
 
 export default useLobby;
