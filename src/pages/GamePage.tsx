5eb2088f7ff8bdee8abb6c9949b925cc7adc16b4
mtaghizada <magatagi0@gmail.com>
2023-02-01 23:26:39 +0400

    useGame hook was removed from the GamePage.tsx and GameProvider was added to the GamePage

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,18 +1,16 @@
 import React from "react";
 import { useParams } from "react-router";
-import PageLoader from "../components/PageLoader";
-import useGame from "../hooks/useGame";
+import GameProvider from "../contexts/GameProvider";
 
 const GamePage = () => {
   const { id } = useParams();
-  const { game, isLoading } = useGame(id as unknown as string);
-
-  if (isLoading) return <PageLoader />;
 
   return (
-    <div className="center-content full-screen font-bold">
-      <h1>Game Page: {id}</h1>
-    </div>
+    <GameProvider id={id as unknown as string}>
+      <div className="center-content full-screen font-bold">
+        <h1>Game Page: {id}</h1>
+      </div>
+    </GameProvider>
   );
 };
 
